5b91f025ee1cc27d98c36d767ba715bf09dc244d
sangtran <sang56pm@gmail.com>
2024-07-07 15:32:29 +0700

    fix bug

--- infrastructure/vpc-and-database/ec2.tf
+++ infrastructure/vpc-and-database/ec2.tf
@@ -11,11 +11,11 @@ resource "aws_instance" "primary_db_access" {
     aws_cloudformation_stack.primary, aws_cloudformation_stack.secondary
   ]
 
-  ami           = "ami-07832e309d3f756c8" // al2023-ami-2023.5.20240701.0-kernel-6.1-arm64
+  ami           = "ami-06c68f701d8090592" // al2023-ami-2023.5.20240701.0-kernel-6.1-x86_64
   instance_type = "t3.micro"
   key_name      = aws_key_pair.primary_ssh_key.key_name
   vpc_security_group_ids = [aws_cloudformation_stack.primary.outputs["ApplicationSecurityGroup"]]
-  subnet_id = aws_cloudformation_stack.primary.outputs["ApplicationSecurityGroup"]
+  subnet_id = split(", ", aws_cloudformation_stack.primary.outputs["PrivateSubnets"])[0]
 
   tags = {
     Name = "DbAccess"
@@ -37,11 +37,11 @@ resource "aws_instance" "secondary_db_access" {
     aws_cloudformation_stack.primary, aws_cloudformation_stack.secondary
   ]
   
-  ami           = "ami-07832e309d3f756c8" // al2023-ami-2023.5.20240701.0-kernel-6.1-arm64
+  ami           = "ami-0604d81f2fd264c7b" // al2023-ami-2023.5.20240701.0-kernel-6.1-x86_64
   instance_type = "t3.micro"
   key_name      = aws_key_pair.secondary_ssh_key.key_name
   vpc_security_group_ids = [aws_cloudformation_stack.secondary.outputs["ApplicationSecurityGroup"]]
-  subnet_id = aws_cloudformation_stack.secondary.outputs["ApplicationSecurityGroup"]
+  subnet_id = split(", ", aws_cloudformation_stack.secondary.outputs["PrivateSubnets"])[0]
 
   tags = {
     Name = "DbAccess"
